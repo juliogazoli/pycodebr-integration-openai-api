d87317ad0859e48c15ef3e8fc2be51f87beeb9e1
Julio Gazoli <julio_gazoli@hotmail.com>
2024-10-29 15:18:38 -0300

    response in stream format

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ client = OpenAI(
     api_key=config('API_KEY'),
 )
 
-response = client.chat.completions.create(
+stream = client.chat.completions.create(
     model='gpt-3.5-turbo',
     messages=[
         {'role': 'user', 'content': 'Fale sobre o Fiat Elba 1988'},
     ],
+    stream=True,
 )
 
-print(response.choices[0].message.content)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end='')
