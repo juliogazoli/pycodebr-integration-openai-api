37bcbb8cb1f8e46c4772132447d8390303206e5c
Julio Gazoli <julio_gazoli@hotmail.com>
2024-10-30 15:04:27 -0300

    context and agents

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ client = OpenAI(
     api_key=config('API_KEY'),
 )
 
-stream = client.chat.completions.create(
+response = client.chat.completions.create(
     model='gpt-3.5-turbo',
     messages=[
-        {'role': 'user', 'content': 'Fale sobre o Fiat Elba 1988'},
+        # {'role': 'system', 'content': 'Você é um assistente virtual especializado em carro e mecânica automotiva. Dê respostas técnicas sobre mecânica.'},
+        # {'role': 'user', 'content': 'Fale sobre o Chevrolet Cobalt'},
+
+        {'role': 'system', 'content': 'Dê respostas técnicas sobre programação. Se comporte como um programador Python experiente, especialista de projetos e arquitetura limpa.'},
+        {'role': 'user', 'content': 'Me mostre como posso fazer um projeto Django com as melhores boas práticas.'},
     ],
-    stream=True,
 )
 
-for chunk in stream:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end='')
+print(response.choices[0].message.content)
